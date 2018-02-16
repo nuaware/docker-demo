3f9d819d06b50266f4423d238b24bb19ef1f357b
Mike Bright <github.com@mjbright.net>
2018-02-16 19:45:19 +0100

    Build/test v5 and v6 also

--- test.sh
+++ test.sh
@@ -6,7 +6,8 @@ press() {
     read
 }
 
-for image in mjbright/docker-demo:1 mjbright/docker-demo:2 mjbright/k8s-demo:1 mjbright/k8s-demo:2; do
+OLD() {
+  for image in mjbright/docker-demo:1 mjbright/docker-demo:2 mjbright/k8s-demo:1 mjbright/k8s-demo:2; do
     echo "Testing image $image"
     docker run -d -p 8080:8080 $image
     ID=$(docker ps -ql)
@@ -22,6 +23,23 @@ for image in mjbright/docker-demo:1 mjbright/docker-demo:2 mjbright/k8s-demo:1 m
     press
 
     docker stop $ID; docker rm $ID; 
+  done
+}
+
+VERSIONS=6
+
+
+for image in mjbright/docker-demo mjbright/k8s-demo; do
+    BASE_PORT=9100
+    [ $image = "mjbright/docker-demo" ] && BASE_PORT=9000
+
+    for version in $(seq $VERSIONS); do
+        let PORT=BASE_PORT+version
+
+        docker pull $image:$version
+        docker run --rm -p ${PORT}:8080 -d $image:$version
+    done
 done
+
 
 
