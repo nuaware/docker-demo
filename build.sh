a9afd11f1ef7f26133695c45fc97d121375e2343
Michael Bright <1880109+mjbright@users.noreply.github.com>
2019-08-27 22:20:09 +0200

    Renamed TEMPLATE_ vars
    Added LIVENESS/READINESS variables
    
    Save main.go to tmp/main_<imagename>.go for reference

--- build.sh
+++ build.sh
@@ -26,16 +26,26 @@ build_image() {
     TXT_IMAGE=$1; shift
     PNG_IMAGE=$1; shift
     IMAGE_NAME_VERSION=$1; shift
+    LIVENESS=$1; shift
+    READINESS=$1; shift
+    # e.g. build_image "docker_blue.txt"   "docker_blue.png"   mjbright/docker-demo:1 "OK" "OK"
 
     IMAGE_VERSION=${IMAGE_NAME_VERSION#*:}
 
     echo; echo "---- Building image $IMAGE_NAME_VERSION [VERSION:$IMAGE_VERSION]"
 
-    sed -e "s/REPLACE_LOGO/$TXT_IMAGE/" \
-        -e "s?IMAGE_NAME_VERSION?$IMAGE_NAME_VERSION?" \
-        -e "s/IMAGE_VERSION/$IMAGE_VERSION/" \
-        -e "s/EXPOSE_PORT/$EXPOSE_PORT/" \
+    sed -e "s/TEMPLATE_REPLACE_LOGO/$TXT_IMAGE/" \
+        -e "s?TEMPLATE_IMAGE_NAME_VERSION?$IMAGE_NAME_VERSION?" \
+        -e "s/TEMPLATE_IMAGE_VERSION/$IMAGE_VERSION/" \
+        -e "s/TEMPLATE_EXPOSE_PORT/$EXPOSE_PORT/" \
+        -e "s/TEMPLATE_LIVENESS/$LIVENESS/" \
+        -e "s/TEMPLATE_READINESS/$READINESS/" \
         demo-main-go.tmpl > main.go
+
+    IMAGE_NAME=${IMAGE_NAME_VERSION%:*}
+    USE_IMAGE_NAME=$(echo $IMAGE_SNAME | sed -e 's?/?_?g')
+    mkdir -p tmp
+    cp -a main.go tmp/main_${USE_IMAGE_NAME}_${IMAGE_VERSION}.go
 
     sed "s/REPLACE_LOGO/$PNG_IMAGE/" templates/index.html.tmpl.tmpl > templates/index.html.tmpl
 
@@ -63,28 +73,44 @@ else
 fi
 
 
-build_image "docker_blue.txt"   "docker_blue.png"   mjbright/docker-demo:1
-build_image "docker_green.txt"  "docker_green.png"  mjbright/docker-demo:2
-build_image "docker_red.txt"    "docker_red.png"    mjbright/docker-demo:3
-build_image "docker_yellow.txt" "docker_yellow.png" mjbright/docker-demo:4
-build_image "docker_cyan.txt"   "docker_cyan.png"   mjbright/docker-demo:5
-build_image "docker_white.txt"  "docker_white.png"  mjbright/docker-demo:6
+build_image "docker_blue.txt"   "docker_blue.png"   mjbright/docker-demo:1 "OK" "OK"
+build_image "docker_green.txt"  "docker_green.png"  mjbright/docker-demo:2 "OK" "OK"
+build_image "docker_red.txt"    "docker_red.png"    mjbright/docker-demo:3 "OK" "OK"
+build_image "docker_yellow.txt" "docker_yellow.png" mjbright/docker-demo:4 "OK" "OK"
+build_image "docker_cyan.txt"   "docker_cyan.png"   mjbright/docker-demo:5 "OK" "OK"
+build_image "docker_white.txt"  "docker_white.png"  mjbright/docker-demo:6 "OK" "OK"
 
-build_image "kubernetes_blue.txt"   "kubernetes_blue.png"   mjbright/k8s-demo:1
-build_image "kubernetes_green.txt"  "kubernetes_green.png"  mjbright/k8s-demo:2
-build_image "kubernetes_red.txt"    "kubernetes_red.png"    mjbright/k8s-demo:3
-build_image "kubernetes_yellow.txt" "kubernetes_yellow.png" mjbright/k8s-demo:4
-build_image "kubernetes_cyan.txt"   "kubernetes_cyan.png"   mjbright/k8s-demo:5
-build_image "kubernetes_white.txt"  "kubernetes_white.png"  mjbright/k8s-demo:6
+build_image "docker_blue.txt"   "docker_blue.png"   mjbright/docker-demo:bad1 "50s" "20s"
+build_image "docker_green.txt"  "docker_green.png"  mjbright/docker-demo:bad2 "50s" "20s"
+build_image "docker_red.txt"    "docker_red.png"    mjbright/docker-demo:bad3 "50s" "20s"
+build_image "docker_yellow.txt" "docker_yellow.png" mjbright/docker-demo:bad4 "50s" "20s"
+build_image "docker_cyan.txt"   "docker_cyan.png"   mjbright/docker-demo:bad5 "50s" "20s"
+build_image "docker_white.txt"  "docker_white.png"  mjbright/docker-demo:bad6 "50s" "20s"
+
+build_image "kubernetes_blue.txt"   "kubernetes_blue.png"   mjbright/k8s-demo:1 "OK" "OK"
+build_image "kubernetes_green.txt"  "kubernetes_green.png"  mjbright/k8s-demo:2 "OK" "OK"
+build_image "kubernetes_red.txt"    "kubernetes_red.png"    mjbright/k8s-demo:3 "OK" "OK"
+build_image "kubernetes_yellow.txt" "kubernetes_yellow.png" mjbright/k8s-demo:4 "OK" "OK"
+build_image "kubernetes_cyan.txt"   "kubernetes_cyan.png"   mjbright/k8s-demo:5 "OK" "OK"
+build_image "kubernetes_white.txt"  "kubernetes_white.png"  mjbright/k8s-demo:6 "OK" "OK"
+
+build_image "kubernetes_blue.txt"   "kubernetes_blue.png"   mjbright/k8s-demo:bad1 "50s" "20s"
+build_image "kubernetes_green.txt"  "kubernetes_green.png"  mjbright/k8s-demo:bad2 "50s" "20s"
+build_image "kubernetes_red.txt"    "kubernetes_red.png"    mjbright/k8s-demo:bad3 "50s" "20s"
+build_image "kubernetes_yellow.txt" "kubernetes_yellow.png" mjbright/k8s-demo:bad4 "50s" "20s"
+build_image "kubernetes_cyan.txt"   "kubernetes_cyan.png"   mjbright/k8s-demo:bad5 "50s" "20s"
+build_image "kubernetes_white.txt"  "kubernetes_white.png"  mjbright/k8s-demo:bad6 "50s" "20s"
 
 for version in $(seq $VERSIONS); do
     docker tag mjbright/k8s-demo:$version  mjbright/ckad-demo:$version
+    docker tag mjbright/k8s-demo:bad$version  mjbright/ckad-demo:bad$version
 done
 
 IMAGES=""
 for image in mjbright/docker-demo mjbright/k8s-demo mjbright/ckad-demo; do
    for version in $(seq $VERSIONS); do
        IMAGES+="${image}:${version} "
+       IMAGES+="${image}:bad${version} "
    done
 done
 
