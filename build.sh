6d14e3577fc19ed52cdd19a223adaf52f4457af1
Michael Bright <1880109+mjbright@users.noreply.github.com>
2020-05-27 10:38:24 +0200

    Updated to build nuaware/k8s-demo

--- build.sh
+++ build.sh
@@ -28,7 +28,7 @@ build_image() {
     IMAGE_NAME_VERSION=$1; shift
     LIVENESS=$1; shift
     READINESS=$1; shift
-    # e.g. build_image "docker_blue.txt"   "docker_blue.png"   mjbright/docker-demo:1 "OK" "OK"
+    # e.g. build_image "docker_blue.txt"   "docker_blue.png"   nuaware/docker-demo:1 "OK" "OK"
 
     IMAGE_VERSION=${IMAGE_NAME_VERSION#*:}
 
@@ -78,56 +78,60 @@ else
     docker login
 fi
 
+IGNORE='''
+build_image "docker_blue.txt"   "docker_blue.png"   nuaware/docker-demo:1 "OK" "OK"
+build_image "docker_green.txt"  "docker_green.png"  nuaware/docker-demo:2 "OK" "OK"
+build_image "docker_red.txt"    "docker_red.png"    nuaware/docker-demo:3 "OK" "OK"
+build_image "docker_yellow.txt" "docker_yellow.png" nuaware/docker-demo:4 "OK" "OK"
+build_image "docker_cyan.txt"   "docker_cyan.png"   nuaware/docker-demo:5 "OK" "OK"
+build_image "docker_white.txt"  "docker_white.png"  nuaware/docker-demo:6 "OK" "OK"
 
-build_image "docker_blue.txt"   "docker_blue.png"   mjbright/docker-demo:1 "OK" "OK"
-build_image "docker_green.txt"  "docker_green.png"  mjbright/docker-demo:2 "OK" "OK"
-build_image "docker_red.txt"    "docker_red.png"    mjbright/docker-demo:3 "OK" "OK"
-build_image "docker_yellow.txt" "docker_yellow.png" mjbright/docker-demo:4 "OK" "OK"
-build_image "docker_cyan.txt"   "docker_cyan.png"   mjbright/docker-demo:5 "OK" "OK"
-build_image "docker_white.txt"  "docker_white.png"  mjbright/docker-demo:6 "OK" "OK"
+build_image "docker_blue.txt"   "docker_blue.png"   nuaware/docker-demo:bad1 "50s" "20s"
+build_image "docker_green.txt"  "docker_green.png"  nuaware/docker-demo:bad2 "50s" "20s"
+build_image "docker_red.txt"    "docker_red.png"    nuaware/docker-demo:bad3 "50s" "20s"
+build_image "docker_yellow.txt" "docker_yellow.png" nuaware/docker-demo:bad4 "50s" "20s"
+build_image "docker_cyan.txt"   "docker_cyan.png"   nuaware/docker-demo:bad5 "50s" "20s"
+build_image "docker_white.txt"  "docker_white.png"  nuaware/docker-demo:bad6 "50s" "20s"
+'''
 
-build_image "docker_blue.txt"   "docker_blue.png"   mjbright/docker-demo:bad1 "50s" "20s"
-build_image "docker_green.txt"  "docker_green.png"  mjbright/docker-demo:bad2 "50s" "20s"
-build_image "docker_red.txt"    "docker_red.png"    mjbright/docker-demo:bad3 "50s" "20s"
-build_image "docker_yellow.txt" "docker_yellow.png" mjbright/docker-demo:bad4 "50s" "20s"
-build_image "docker_cyan.txt"   "docker_cyan.png"   mjbright/docker-demo:bad5 "50s" "20s"
-build_image "docker_white.txt"  "docker_white.png"  mjbright/docker-demo:bad6 "50s" "20s"
+build_image "kubernetes_blue.txt"   "kubernetes_blue.png"   nuaware/k8s-demo:1 "OK" "OK"
+build_image "kubernetes_green.txt"  "kubernetes_green.png"  nuaware/k8s-demo:2 "OK" "OK"
+build_image "kubernetes_red.txt"    "kubernetes_red.png"    nuaware/k8s-demo:3 "OK" "OK"
+build_image "kubernetes_yellow.txt" "kubernetes_yellow.png" nuaware/k8s-demo:4 "OK" "OK"
+build_image "kubernetes_cyan.txt"   "kubernetes_cyan.png"   nuaware/k8s-demo:5 "OK" "OK"
+build_image "kubernetes_white.txt"  "kubernetes_white.png"  nuaware/k8s-demo:6 "OK" "OK"
 
-build_image "kubernetes_blue.txt"   "kubernetes_blue.png"   mjbright/k8s-demo:1 "OK" "OK"
-build_image "kubernetes_green.txt"  "kubernetes_green.png"  mjbright/k8s-demo:2 "OK" "OK"
-build_image "kubernetes_red.txt"    "kubernetes_red.png"    mjbright/k8s-demo:3 "OK" "OK"
-build_image "kubernetes_yellow.txt" "kubernetes_yellow.png" mjbright/k8s-demo:4 "OK" "OK"
-build_image "kubernetes_cyan.txt"   "kubernetes_cyan.png"   mjbright/k8s-demo:5 "OK" "OK"
-build_image "kubernetes_white.txt"  "kubernetes_white.png"  mjbright/k8s-demo:6 "OK" "OK"
+IGNORE='''
+build_image "kubernetes_blue.txt"   "kubernetes_blue.png"   nuaware/k8s-demo:bad1 "50s" "20s"
+build_image "kubernetes_green.txt"  "kubernetes_green.png"  nuaware/k8s-demo:bad2 "50s" "20s"
+build_image "kubernetes_red.txt"    "kubernetes_red.png"    nuaware/k8s-demo:bad3 "50s" "20s"
+build_image "kubernetes_yellow.txt" "kubernetes_yellow.png" nuaware/k8s-demo:bad4 "50s" "20s"
+build_image "kubernetes_cyan.txt"   "kubernetes_cyan.png"   nuaware/k8s-demo:bad5 "50s" "20s"
+build_image "kubernetes_white.txt"  "kubernetes_white.png"  nuaware/k8s-demo:bad6 "50s" "20s"
 
-build_image "kubernetes_blue.txt"   "kubernetes_blue.png"   mjbright/k8s-demo:bad1 "50s" "20s"
-build_image "kubernetes_green.txt"  "kubernetes_green.png"  mjbright/k8s-demo:bad2 "50s" "20s"
-build_image "kubernetes_red.txt"    "kubernetes_red.png"    mjbright/k8s-demo:bad3 "50s" "20s"
-build_image "kubernetes_yellow.txt" "kubernetes_yellow.png" mjbright/k8s-demo:bad4 "50s" "20s"
-build_image "kubernetes_cyan.txt"   "kubernetes_cyan.png"   mjbright/k8s-demo:bad5 "50s" "20s"
-build_image "kubernetes_white.txt"  "kubernetes_white.png"  mjbright/k8s-demo:bad6 "50s" "20s"
+build_image "kubernetes_blue.txt"   "kubernetes_blue.png"   nuaware/ckad-demo:1 "OK" "OK"
+build_image "kubernetes_green.txt"  "kubernetes_green.png"  nuaware/ckad-demo:2 "OK" "OK"
+build_image "kubernetes_red.txt"    "kubernetes_red.png"    nuaware/ckad-demo:3 "OK" "OK"
+build_image "kubernetes_yellow.txt" "kubernetes_yellow.png" nuaware/ckad-demo:4 "OK" "OK"
+build_image "kubernetes_cyan.txt"   "kubernetes_cyan.png"   nuaware/ckad-demo:5 "OK" "OK"
+build_image "kubernetes_white.txt"  "kubernetes_white.png"  nuaware/ckad-demo:6 "OK" "OK"
 
-build_image "kubernetes_blue.txt"   "kubernetes_blue.png"   mjbright/ckad-demo:1 "OK" "OK"
-build_image "kubernetes_green.txt"  "kubernetes_green.png"  mjbright/ckad-demo:2 "OK" "OK"
-build_image "kubernetes_red.txt"    "kubernetes_red.png"    mjbright/ckad-demo:3 "OK" "OK"
-build_image "kubernetes_yellow.txt" "kubernetes_yellow.png" mjbright/ckad-demo:4 "OK" "OK"
-build_image "kubernetes_cyan.txt"   "kubernetes_cyan.png"   mjbright/ckad-demo:5 "OK" "OK"
-build_image "kubernetes_white.txt"  "kubernetes_white.png"  mjbright/ckad-demo:6 "OK" "OK"
-
-build_image "kubernetes_blue.txt"   "kubernetes_blue.png"   mjbright/ckad-demo:bad1 "50s" "20s"
-build_image "kubernetes_green.txt"  "kubernetes_green.png"  mjbright/ckad-demo:bad2 "50s" "20s"
-build_image "kubernetes_red.txt"    "kubernetes_red.png"    mjbright/ckad-demo:bad3 "50s" "20s"
-build_image "kubernetes_yellow.txt" "kubernetes_yellow.png" mjbright/ckad-demo:bad4 "50s" "20s"
-build_image "kubernetes_cyan.txt"   "kubernetes_cyan.png"   mjbright/ckad-demo:bad5 "50s" "20s"
-build_image "kubernetes_white.txt"  "kubernetes_white.png"  mjbright/ckad-demo:bad6 "50s" "20s"
+build_image "kubernetes_blue.txt"   "kubernetes_blue.png"   nuaware/ckad-demo:bad1 "50s" "20s"
+build_image "kubernetes_green.txt"  "kubernetes_green.png"  nuaware/ckad-demo:bad2 "50s" "20s"
+build_image "kubernetes_red.txt"    "kubernetes_red.png"    nuaware/ckad-demo:bad3 "50s" "20s"
+build_image "kubernetes_yellow.txt" "kubernetes_yellow.png" nuaware/ckad-demo:bad4 "50s" "20s"
+build_image "kubernetes_cyan.txt"   "kubernetes_cyan.png"   nuaware/ckad-demo:bad5 "50s" "20s"
+build_image "kubernetes_white.txt"  "kubernetes_white.png"  nuaware/ckad-demo:bad6 "50s" "20s"
+'''
 
 #for version in $(seq $VERSIONS); do
-#    docker tag mjbright/k8s-demo:$version  mjbright/ckad-demo:$version
-#    docker tag mjbright/k8s-demo:bad$version  mjbright/ckad-demo:bad$version
+#    docker tag nuaware/k8s-demo:$version  nuaware/ckad-demo:$version
+#    docker tag nuaware/k8s-demo:bad$version  nuaware/ckad-demo:bad$version
 #done
 
 IMAGES=""
-for image in mjbright/docker-demo mjbright/k8s-demo mjbright/ckad-demo; do
+#for image in nuaware/docker-demo nuaware/k8s-demo nuaware/ckad-demo; do
+for image in nuaware/k8s-demo; do
    for version in $(seq $VERSIONS); do
        IMAGES+="${image}:${version} "
        IMAGES+="${image}:bad${version} "
--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-docker run -d -p 4321:4321 mjbright/docker-demo:20
-docker run -d -p 4322:4321 mjbright/docker-demo:21
+docker run -d -p 4321:4321 nuaware/ckad-demo:1
+docker run -d -p 4322:4321 nuaware/ckad-demo:2
 
 curl 127.0.0.1:4321
 #read
